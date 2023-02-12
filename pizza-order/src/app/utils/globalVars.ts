fbaf07417f72aab7cffecfa33c05f418f341ae7b
swbonjour <71038541+swbonjour@users.noreply.github.com>
2023-02-12 17:24:46 +0300

    'prod'

--- pizza-order/src/app/utils/globalVars.ts
+++ pizza-order/src/app/utils/globalVars.ts
@@ -1,2 +1,1 @@
-export const API_URL = 'http://localhost:3000/api'
-export const SLACK_WEBHOOK = 'https://hooks.slack.com/services/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'
+export const API_URL = 'https://parsimonious-vacation-production.up.railway.app/api'
